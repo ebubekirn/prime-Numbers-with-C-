a3db4d984a3d1ad382540ce1670a402f1e0ffa5b
Ebubekir <ebubekirn96@gmail.com>
2022-09-27 16:15:57 +0300

    asal sayı bulma
    Kullanıcının girdiği sayının asal olup olmadığını bulan kod. Ardında sonraki 5 asal sayıyı yazdır.

---
+++ asalSayiBulma/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asalSayiBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            Console.Write("Sayı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 0 || sayi == 1)
+            {
+                Console.WriteLine("Girilen sayı asal değildir.");
+            }
+            else
+            {
+                for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        sayac++;
+
+                    }
+                }
+                if (sayac == 0)
+                {
+                    Console.WriteLine("{0} sayısı asal sayıdır.", sayi);
+                }
+                else
+                {
+                    Console.WriteLine("{0} sayısı asal sayı değildir.", sayi);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+
+
+        
+    }
+}
